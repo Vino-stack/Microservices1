2d7aa94d55d9721d94abcb45214781759ae4e7af
Vino-stack <66718873+Vino-stack@users.noreply.github.com>
2020-07-06 01:21:36 -0700

    Admin-server

---
+++ admin-server/src/test/java/com/pradeep/adminserver/AdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradeep.adminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
